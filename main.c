8da406c183b295b0b5e0ddbc9872a98703742645
Akutsal23 <akutsal23@ku.edu.tr>
2025-07-12 02:07:45 +0300

    moving player

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include "engine/engine.h"
 #include "engine/renderer.h"
-
+#include "game/game.h"
 #include <stdio.h>
 
 int main(void)
 {
+    /*
     if (!engine_init("Renderer Test", 800, 600)) return 1;
 
     Texture* tex = renderer_load_texture("assets/player.png");
@@ -25,4 +26,5 @@ int main(void)
     renderer_free_texture(tex);
     engine_shutdown();
     return 0;
+    */
 }
