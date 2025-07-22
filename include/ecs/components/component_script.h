d1df7346badf907966ac71eae80b295ea2fb6307
Akutsal23 <akutsal23@ku.edu.tr>
2025-07-22 16:44:36 +0300

    scripts

---
+++ include/ecs/components/component_script.h
@@ -0,0 +1,8 @@
+//
+// Created by Ahmet Eren Kutsal on 21.07.2025.
+//
+
+#ifndef COMPONENT_SCRIPT_H
+#define COMPONENT_SCRIPT_H
+
+#endif //COMPONENT_SCRIPT_H
---
+++ src/ecs/components/component_script.c
@@ -0,0 +1,3 @@
+//
+// Created by Ahmet Eren Kutsal on 21.07.2025.
+//
